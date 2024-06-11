ac5b9ff37d41cf7ad9b77517bcaab672809f1d01
Evan Jackson <evan.jackson@torqit.ca>
2024-06-11 12:53:52 -0400

    Adding extra condition on pimcore-install

--- .docker/init/init.sh
+++ .docker/init/init.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# TODO extra condition to make extra-sure this doesn't run in non-local envs
 if [ "$(mysql -h "$DATABASE_HOST" -u "$DATABASE_USER" -p"$DATABASE_PASSWORD" \
-      -sse "select count(*) from information_schema.tables where table_schema='pimcore' and table_name='assets';")" -eq 0 ]
+      -sse "select count(*) from information_schema.tables where table_schema='pimcore' and table_name='assets';")" -eq 0 ] \
+   && [ $PIMCORE_INSTALL ]
 then
   echo "Database is empty, so doing a fresh install to seed the database..."
   runuser -u www-data -- vendor/bin/pimcore-install --skip-database-config --no-interaction
